23fd3ce1e6307278f2f6fd283e57b75e07128a0f
sidineiPortugal <sidineiportugal@hotmail.com>
2021-06-16 03:52:33 -0300

    estruturação do header/footer, cards da home e primeiras requisições adicionadas

--- public/app.js
+++ public/app.js
@@ -1,10 +1,43 @@
 var app = angular.module('linx_brewery', ['ngRoute']);
 
+//Configurando as rotas da aplicação
 app.config(['$routeProvider', (($routeProvider)=>{
     $routeProvider.when('/',{
-        template:"<h1>Sidinei</h1>"
+        templateUrl:"public/templates/home.html",
+        controller:'app_controller'
     }).otherwise({
         redirectTo:'/notfound',
-        template:"<h1>Pagina não encontrada</h1>"
+        templateUrl:"public/templates/detail.html"
     });
 })]);
+
+//Rotas externas da aplicação
+app.service('breweryDB', ['$http', function($http){
+
+    let breweries_url = 'https://api.openbrewerydb.org/breweries';
+
+    let list_breweries = (()=>{
+        return $http.get(breweries_url);
+    });
+    let detail_brewerie = ((id)=>{
+        return $http.get(breweries_url + '/' + id);
+    });
+    return {
+        list_breweries: list_breweries,
+        detail_brewerie: detail_brewerie
+    };
+    
+}]);
+
+//Controllers
+app.controller('app_controller', ['$scope', 'breweryDB' , function($scope, breweryDB){
+    $scope.breweries = [];
+    $scope.list = function(){
+        breweryDB.list_breweries().then((response)=>{
+            $scope.breweries = response.data
+            console.log(response.data);
+        })
+    }, function(erros){ console.log(erros);};
+
+    $scope.list();
+}]);
